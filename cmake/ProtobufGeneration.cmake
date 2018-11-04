8b4221cbbd49c47baed9e86ce85c9c83c78029e7
MathieuNassar <Mathieu.Nassar@gmail.com>
2018-11-04 08:27:42 +0100

    CHG: updated protobuf generation script to skip grpc if not found

--- cmake/ProtobufGeneration.cmake
+++ cmake/ProtobufGeneration.cmake
@@ -1,9 +1,15 @@
 function(generate_protoc source_dir output_dir source_file)
 	message(STATUS "Generating grpc and protobuf: " ${source_file})
 
+	if (GRPC_CPP_PLUGIN_PATH)
+		execute_process(
+			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+			COMMAND ${PROTOC_PATH} -I ${Protobuf_INCLUDE_DIRS} -I ${source_dir} --grpc_out=${output_dir} --plugin=protoc-gen-grpc=${GRPC_CPP_PLUGIN_PATH} ${source_file}
+		)
+	endif()
+	
 	execute_process(
 		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-		COMMAND ${PROTOC_PATH} -I ${Protobuf_INCLUDE_DIRS} -I ${source_dir} --grpc_out=${output_dir} --plugin=protoc-gen-grpc=${GRPC_CPP_PLUGIN_PATH} ${source_file}
 		COMMAND ${PROTOC_PATH} -I ${Protobuf_INCLUDE_DIRS} -I ${source_dir} --cpp_out=${output_dir} ${source_file}
 	)
 endfunction(generate_protoc)
