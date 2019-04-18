3cc51d0825d44a29859f0a3f5432ad72a143ba60
MathieuNassar <Mathieu.Nassar@gmail.com>
2019-04-18 22:33:41 +0200

    CHG: made proto generation able to compile list of files

--- cmake/ProtobufGeneration.cmake
+++ cmake/ProtobufGeneration.cmake
@@ -1,27 +1,34 @@
 function(generate_protoc source_dir output_dir source_file)
-	message(STATUS "Generating grpc and protobuf: " ${source_file})
+	# generate the list of include folders
+	foreach(dir ${source_dir})
+		set(proto_include_dirs ${proto_include_dirs} -I ${dir})
+	endforeach()
 
-	if (GRPC_CPP_PLUGIN_PATH)
+	foreach(file ${source_file})
+		message(STATUS "Generating grpc and protobuf: ${file}")
+
+		if (GRPC_CPP_PLUGIN_PATH)
+			execute_process(
+				WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+				COMMAND ${PROTOC_PATH} -I ${Protobuf_INCLUDE_DIRS} ${proto_include_dirs} --grpc_out=${output_dir} --plugin=protoc-gen-grpc=${GRPC_CPP_PLUGIN_PATH} ${file}
+				RESULT_VARIABLE protoc_grpc_result
+				ERROR_VARIABLE protoc_grpc_error_variable
+			)
+
+			if (NOT protoc_grpc_result EQUAL "0")
+				message(FATAL_ERROR "Failed to generate protobuf. Error: ${protoc_grpc_error_variable}")
+			endif()
+		endif()
+		
 		execute_process(
 			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-			COMMAND ${PROTOC_PATH} -I ${Protobuf_INCLUDE_DIRS} -I ${source_dir} --grpc_out=${output_dir} --plugin=protoc-gen-grpc=${GRPC_CPP_PLUGIN_PATH} ${source_file}
-			RESULT_VARIABLE protoc_grpc_result
-			ERROR_VARIABLE protoc_grpc_error_variable
+			COMMAND ${PROTOC_PATH} -I ${Protobuf_INCLUDE_DIRS} -I ${source_dir} --cpp_out=${output_dir} ${file}
+			RESULT_VARIABLE protoc_result
+			ERROR_VARIABLE protoc_error_variable
 		)
 
-		if (NOT protoc_grpc_result EQUAL "0")
-			message(FATAL_ERROR "Failed to generate protobuf. Error: ${protoc_grpc_error_variable}")
+		if (NOT protoc_result EQUAL "0")
+			message(FATAL_ERROR "Failed to generate protobuf. Error: ${protoc_error_variable}")
 		endif()
-	endif()
-	
-	execute_process(
-		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-		COMMAND ${PROTOC_PATH} -I ${Protobuf_INCLUDE_DIRS} -I ${source_dir} --cpp_out=${output_dir} ${source_file}
-		RESULT_VARIABLE protoc_result
-		ERROR_VARIABLE protoc_error_variable
-	)
-
-	if (NOT protoc_result EQUAL "0")
-		message(FATAL_ERROR "Failed to generate protobuf. Error: ${protoc_error_variable}")
-	endif()
+	endforeach()
 endfunction(generate_protoc)
