84b3f23ff6ae37e706f74ce2e8996cb14c707dfa
MathieuNassar <mathieu.nassar@gmail.com>
2019-06-06 22:43:50 +0200

    CHG: switched to conan dependency management

--- cmake/Ghost.cmake
+++ cmake/Ghost.cmake
@@ -17,18 +17,3 @@ function(fetch_ghost_library name url branch)
 	
 	message(STATUS "Fetching ghost library '" ${name} "' done.")
 endfunction(fetch_ghost_library)
-
-function(setup_ghost_repository)
-	#set up the location of ghost_third_parties
-	if (NOT DEFINED ENV{GHOST_THIRD_PARTIES_URL})
-		message(FATAL_ERROR "Ghost third parties location unkown. Please set an environment variable named \"GHOST_THIRD_PARTIES_URL\" containing its URL")
-	endif()
-	
-	set(GHOST_THIRD_PARTIES_ROOT_DIR $ENV{GHOST_THIRD_PARTIES_URL} CACHE STRING "Ghost Third Parties location" FORCE)
-
-	if (EXISTS ${GHOST_THIRD_PARTIES_ROOT_DIR})
-		message(STATUS "Set the URL of ghost third parties to ${GHOST_THIRD_PARTIES_ROOT_DIR}")
-	else()
-		message(FATAL_ERROR "The provided URL for ghost third parties does not exist, aborting.")
-	endif()
-endfunction(setup_ghost_repository)
